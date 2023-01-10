373f8d6ff26b2b6b8b955bf9c7de586c3c5553a2
Manoj Dayaram <m@noj.cc>
2023-01-10 15:14:18 -0800

    adding a filtering stage

--- parser.py
+++ parser.py
@@ -10,3 +10,21 @@ plt = pyplot
 from IPython.core.pylabtools import figsize, getfigs
 from pylab import *
 from numpy import *
+
+# Parsing mbox file guide from here:
+#  https://pysd-cookbook.readthedocs.io/en/latest/data/Emails/Email_Data_Formatter.html
+
+import mailbox
+import pandas as pd
+
+print('Opening file...')
+mb = mailbox.mbox('gmail.mbox')
+print('...done')
+
+keys = ['Date', 'X-Gmail-Labels', 'X-GM-THRID']
+print('Popping a random item...')
+(key, message) = mb.popitem()
+print('..done')
+print('key: ', key)
+print('message:')
+print(message)
