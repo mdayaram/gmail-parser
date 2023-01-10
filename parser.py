5ebc52098bd4bfd08167ffc8d2eeb3960d78bf03
Manoj Dayaram <m@noj.cc>
2023-01-10 14:16:49 -0800

    init

---
+++ parser.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+# simulates %pylab inline
+# as per here: https://stackoverflow.com/questions/20961287/what-is-pylab
+import numpy
+import matplotlib
+from matplotlib import pylab, mlab, pyplot
+np = numpy
+plt = pyplot
+from IPython.core.pylabtools import figsize, getfigs
+from pylab import *
+from numpy import *
