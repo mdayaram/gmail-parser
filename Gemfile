4123acbc966a5a3e75b783e6a49dc1b1a2a4099e
Manoj Dayaram <m@noj.cc>
2023-01-12 15:59:22 -0800

    Adding pry

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source "https://rubygems.org"
 
 gem "mbox"
+gem "pry-byebug"
--- filter.rb
+++ filter.rb
@@ -2,9 +2,13 @@
 
 require "rubygems"
 require "bundler/setup"
+require "pry-byebug"
 require "mbox"
 
 $stdout.sync = true
 
 mbox = Mbox.open("gmail.mbox")
 puts "Number of messages: #{mbox.length}"
+binding.pry
+
+puts "End of script."
