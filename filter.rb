122f37ae1d60f477d4c329003eb40a9d1e076102
Manoj Dayaram <m@noj.cc>
2023-01-12 15:27:01 -0800

    using mbox library

--- filter.rb
+++ filter.rb
@@ -1,48 +1,10 @@
 #!/usr/bin/env ruby
 
+require "rubygems"
+require "bundler/setup"
+require "mbox"
+
 $stdout.sync = true
-label_filter = 'people'
-current_message = []
 
-prev_newline = true
-save_message = false
-num_messages = 0
-saved_num_messages = 0
-
-outfile = File.open(label_filter + ".mbox", "a")
-
-puts "opening file"
-File.open("gmail.mbox") do |f|
-	puts "file open, going through lines"
-
-	f.each_line.with_index do |line, lineno|
-		line.strip!
-		if line.start_with?("From:") && prev_newline
-			# start of a new message
-			puts "Processed ##{num_messages}"
-			puts "Saved ##{saved_num_messages}"
-			num_messages += 1
-			if save_message
-				saved_num_messages += 1
-				current_message.each do |l|
-					outfile.write(l + "\n")
-					outfile.flush
-				end
-				exit 1
-			end
-			current_message = []
-			save_message = false
-			GC.start
-		end
-
-
-		if line.start_with?("X-Gmail-Labels:")
-			if line.downcase.include?(label_filter)
-				save_message = true
-			end
-		end
-
-		current_message << line
-		prev_newline = line.empty?
-	end
-end
+mbox = Mbox.open("gmail.mbox")
+puts "Number of messages: #{mbox.length}"
