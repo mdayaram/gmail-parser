d4e8c53279601258d85bd42750b353f18c2ed15c
Manoj Dayaram <m@noj.cc>
2023-02-05 11:51:50 -0800

    Format everything to two spaces

--- filter.rb
+++ filter.rb
@@ -8,24 +8,24 @@ require "./joplin_api"
 
 
 def cli
-    @cli ||= HighLine.new
+  @cli ||= HighLine.new
 end
 def joplin
-    raise "Please set the JOKEN env variable" if ENV["JOKEN"].nil?
-    @jclient ||= JoplinApi.new(ENV["JOKEN"])
+  raise "Please set the JOKEN env variable" if ENV["JOKEN"].nil?
+  @jclient ||= JoplinApi.new(ENV["JOKEN"])
 end
 def prompt_to_continue?(msg = "\nContinue? ")
-    if !cli.agree(msg)
-        cli.say "Exiting..."
-        exit 1
-    end
+  if !cli.agree(msg)
+    cli.say "Exiting..."
+    exit 1
+  end
 end
 def decode_quoted_printable(text)
   text.unpack("M").first.encode("utf-8", "iso-8859-1")
 end
 
 if ARGV[0].nil? || ARGV[0].empty?
-    abort("ERROR: Please provide a path to an mbox file as the argument.")
+  abort("ERROR: Please provide a path to an mbox file as the argument.")
 end
 
 # Initialize joplin client.
@@ -47,18 +47,18 @@ cli.say "\nPlease type in the appropriate Joplin tag to use for each label...\n"
 labels_to_tags = {}
 ignored_labels = []
 labels.each do |l|
-    tag = (cli.ask "Tag for \"#{l}\"?").downcase
-    if !tag.empty?
-      labels_to_tags[l] = tag
-    else
-      ignored_labels << l
-    end
+  tag = (cli.ask "Tag for \"#{l}\"?").downcase
+  if !tag.empty?
+    labels_to_tags[l] = tag
+  else
+    ignored_labels << l
+  end
 end
 
 cli.say "\nWe'll use the following mapping for labels to tags (note: DNE means the tag does not exist and would be created):"
 labels_to_tags.each do |l, t|
-    current_tag = joplin.find_tag(t) || {"id" => "DNE"}
-    cli.say "#{l} => #{t} - ID: #{current_tag["id"]}"
+  current_tag = joplin.find_tag(t) || {"id" => "DNE"}
+  cli.say "#{l} => #{t} - ID: #{current_tag["id"]}"
 end
 ignored_labels.each do |l|
   cli.say "#{l} => IGNORED"
@@ -67,52 +67,52 @@ prompt_to_continue?
 
 cli.say "\nVerify that every email/note looks correct:\n"
 conversations.values.each_with_index do |messages, index|
-    title = messages[0].subject
-    date = messages[0].date
-    labels = messages.map(&:gmail_labels).flatten.uniq
-    tags = labels.map { |l| labels_to_tags[l] }.compact
-    body_l = []
-    messages.each do |m|
-        if m.is_self_note?
-          body_l << m.text_body
-        else
-          body_l << "From: #{m.from}\nTo: #{m.to}\n\n" + m.text_body
-        end
+  title = messages[0].subject
+  date = messages[0].date
+  labels = messages.map(&:gmail_labels).flatten.uniq
+  tags = labels.map { |l| labels_to_tags[l] }.compact
+  body_l = []
+  messages.each do |m|
+    if m.is_self_note?
+      body_l << m.text_body
+    else
+      body_l << "From: #{m.from}\nTo: #{m.to}\n\n" + m.text_body
     end
-    body = body_l.map(&:strip).join("\n\n--\n\n")
+  end
+  body = body_l.map(&:strip).join("\n\n--\n\n")
 
-    cli.say "\n\n======== ENTRY #{index + 1}/#{conversations.size} ========="
-    cli.say "Title: #{title}"
-    cli.say "Date: #{date}"
-    cli.say "Tags: #{tags} #{tags.size > 1 ? "- !!WARNING!! - MULTIPLE TAGS!": ""}\n"
-    cli.say "Body:"
-    puts body # cli.say has some issues printing some email bodies.
+  cli.say "\n\n======== ENTRY #{index + 1}/#{conversations.size} ========="
+  cli.say "Title: #{title}"
+  cli.say "Date: #{date}"
+  cli.say "Tags: #{tags} #{tags.size > 1 ? "- !!WARNING!! - MULTIPLE TAGS!": ""}\n"
+  cli.say "Body:"
+  puts body # cli.say has some issues printing some email bodies.
 
-    cli.say "\n---\nWant to keep this entry?"
-    response = cli.choose do |menu|
-        menu.prompt = "Choose an action: "
-        menu.choice(:upload)
-        menu.choice(:upload_quote_printable_decoded)
-        menu.choice(:skip)
-        menu.choice(:quit)
-    end
+  cli.say "\n---\nWant to keep this entry?"
+  response = cli.choose do |menu|
+    menu.prompt = "Choose an action: "
+    menu.choice(:upload)
+    menu.choice(:upload_quote_printable_decoded)
+    menu.choice(:skip)
+    menu.choice(:quit)
+  end
 
-    case response
-    when :upload
-        cli.say "You chose to upload, beginning upload process..."
-        joplin.create_note(title: title, body: body, date: date, tags: tags)
-        cli.say "...done!"
-    when :upload_quote_printable_decoded
-        cli.say "You chose to decode quoted printable and then upload..."
-        body = decode_quoted_printable(body)
-        joplin.create_note(title: title, body: body, date: date, tags: tags)
-        cli.say "...done!"
-    when :skip
-        cli.say "Skipping entry #{index + 1}..."
-    when :quit
-        cli.say "Quitting..."
-        exit 1
-    end
+  case response
+  when :upload
+    cli.say "You chose to upload, beginning upload process..."
+    joplin.create_note(title: title, body: body, date: date, tags: tags)
+    cli.say "...done!"
+  when :upload_quote_printable_decoded
+    cli.say "You chose to decode quoted printable and then upload..."
+    body = decode_quoted_printable(body)
+    joplin.create_note(title: title, body: body, date: date, tags: tags)
+    cli.say "...done!"
+  when :skip
+    cli.say "Skipping entry #{index + 1}..."
+  when :quit
+    cli.say "Quitting..."
+    exit 1
+  end
 end
 
 cli.say "\n\nDone with all entries!"
--- message.rb
+++ message.rb
@@ -7,66 +7,66 @@ require "date"
 require "pry-byebug"
 
 class Message
-    attr_reader :gmail_thread_id, :gmail_labels
-    attr_reader :from, :to, :date, :subject
-    attr_reader :text_body, :html_body
+  attr_reader :gmail_thread_id, :gmail_labels
+  attr_reader :from, :to, :date, :subject
+  attr_reader :text_body, :html_body
 
-    def initialize(mbox_message)
-        @gmail_thread_id = mbox_message.headers["X-Gm-Thrid"]
-        @gmail_labels = mbox_message.headers["X-Gmail-Labels"]&.split(",")&.map(&:strip)
-        @from = mbox_message.headers["From"]
-        @to = mbox_message.headers["To"]
-        @date = parse_date(mbox_message.headers["Date"])
-        @subject = mbox_message.headers["Subject"]
-        @text_body = ""
-        @html_body = ""
+  def initialize(mbox_message)
+    @gmail_thread_id = mbox_message.headers["X-Gm-Thrid"]
+    @gmail_labels = mbox_message.headers["X-Gmail-Labels"]&.split(",")&.map(&:strip)
+    @from = mbox_message.headers["From"]
+    @to = mbox_message.headers["To"]
+    @date = parse_date(mbox_message.headers["Date"])
+    @subject = mbox_message.headers["Subject"]
+    @text_body = ""
+    @html_body = ""
 
-        mbox_message.content.each do |c|
-            if c.headers[:content_type].mime == "text/plain"
-                @text_body += c.content
-            elsif c.headers[:content_type].mime == "text/html"
-                @html_body += c.content
-            else
-                # ignore other content types, probably just metadata.
-                # e.g. "multipart/alternative", "multipart/mixed"
-            end
-        end
-    end
-
-    def is_self_note?
-        emails = ["m@noj.cc", "i.am.noj@gmail.com", "noj@alumni.cmu.edu", "noj@squareup.com", "noj@moovweb.com"]
-        emails.any? { |e| from.include?(e) } && emails.any? { |e| to.include?(e) }
-    end
-
-    def parse_date(date_str)
-      return date_str if date_str.nil? || date_str.empty?
-
-      possible_formats = [
-        "%a, %e %b %Y %H:%M:%S %z", # Mon, 7 Dec 2015 00:28:51 -0800
-        "%e %b %Y %H:%M:%S %z",     # 7 Dec 2015 00:28:51 -0800
-      ]
-
-      possible_formats.each do |format|
-        begin
-          return DateTime.strptime(date_str, format)
-        rescue => _
-        end
+    mbox_message.content.each do |c|
+      if c.headers[:content_type].mime == "text/plain"
+        @text_body += c.content
+      elsif c.headers[:content_type].mime == "text/html"
+        @html_body += c.content
+      else
+        # ignore other content types, probably just metadata.
+        # e.g. "multipart/alternative", "multipart/mixed"
       end
+    end
+  end
 
-      raise "Could not parse date: #{date_str}"
+  def is_self_note?
+    emails = ["m@noj.cc", "i.am.noj@gmail.com", "noj@alumni.cmu.edu", "noj@squareup.com", "noj@moovweb.com"]
+    emails.any? { |e| from.include?(e) } && emails.any? { |e| to.include?(e) }
+  end
+
+  def parse_date(date_str)
+    return date_str if date_str.nil? || date_str.empty?
+
+    possible_formats = [
+      "%a, %e %b %Y %H:%M:%S %z", # Mon, 7 Dec 2015 00:28:51 -0800
+      "%e %b %Y %H:%M:%S %z",     # 7 Dec 2015 00:28:51 -0800
+    ]
+
+    possible_formats.each do |format|
+      begin
+        return DateTime.strptime(date_str, format)
+      rescue => _
+      end
     end
 
-    # Returns a hash of...
-    # { conversation_thread_id => [messages...] }
-    def self.parse_conversations!(mbox_filename)
-        conversations = {}
-        mb = Mbox.open(mbox_filename)
-        mb.each do |m|
-            message = Message.new(m)
-            conversations[message.gmail_thread_id] ||= []
-            conversations[message.gmail_thread_id] << message
-        end
-        conversations.values.each { |c| c.sort_by!(&:date) }
-        conversations
+    raise "Could not parse date: #{date_str}"
+  end
+
+  # Returns a hash of...
+  # { conversation_thread_id => [messages...] }
+  def self.parse_conversations!(mbox_filename)
+    conversations = {}
+    mb = Mbox.open(mbox_filename)
+    mb.each do |m|
+      message = Message.new(m)
+      conversations[message.gmail_thread_id] ||= []
+      conversations[message.gmail_thread_id] << message
     end
+    conversations.values.each { |c| c.sort_by!(&:date) }
+    conversations
+  end
 end
